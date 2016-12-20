dd798a741b8e74c1c9259e50ce0695a369a97940
jmm <jmomarty@gmail.com>
2016-12-20 18:00:15 +0100

    updating 2

--- tsne.bat
+++ tsne.bat
@@ -1,3 +1,1 @@
 rem python -m CorpusViz.demo2 --slow --start 100 --end 300 --perplexity 40 --lr 500 fr.emnlp.1000.txt en.emnlp.1000.txt pt.emnlp.1000.txt
-python -m CorpusViz.demo2 --slow --start 100 --end 300 --perplexity 40 --lr 500 C:\Users\Guillaume\Proxem\Data\Auchan\fr.2gram.sem
-
